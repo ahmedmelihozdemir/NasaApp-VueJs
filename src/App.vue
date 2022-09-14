bb7b2d091595c7bdb865767da8fe2777cb8c2ce0
Melih Özdemir <ozdemirmelihahmed@gmail.com>
2022-09-14 16:31:33 +0300

    Footer add.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
     <v-app class="navbar">
         <HeaderComponent></HeaderComponent>
+        <FooterComponent></FooterComponent>
     </v-app>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import HeaderComponent from "@/components/layout/HeaderComponent.vue";
+import FooterComponent from "./components/layout/Footer.component.vue";
 
 export default Vue.extend({
     name: "Home",
 
     components: {
         HeaderComponent,
+        FooterComponent,
     },
 });
 </script>
